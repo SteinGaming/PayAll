bad975b2e2752a4f85adcdb4eef87b56468abc45
SteinGaming <danialdaryab@gmail.com>
2023-04-10 20:48:17 +0200

    feat: Basic command functionality for 1.19.3 (maybe 1.19.x?)

--- src/main/kotlin/eu/steingaming/payall/PayAll.kt
+++ src/main/kotlin/eu/steingaming/payall/PayAll.kt
@@ -1,14 +1,29 @@
 package eu.steingaming.payall
 
 //import com.mojang.logging.LogUtils
+import com.mojang.brigadier.CommandDispatcher
+import com.mojang.brigadier.arguments.ArgumentType
+import com.mojang.brigadier.arguments.DoubleArgumentType
+import com.mojang.brigadier.arguments.LongArgumentType
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
+import com.mojang.brigadier.builder.RequiredArgumentBuilder
 import kotlinx.coroutines.*
 import net.minecraft.client.Minecraft
+import net.minecraft.commands.CommandSourceStack
+import net.minecraft.commands.Commands
 import net.minecraft.network.chat.Component
+import net.minecraftforge.client.ClientCommandHandler
 import net.minecraftforge.client.event.ClientChatEvent
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent
+import net.minecraftforge.client.event.RegisterClientCommandsEvent
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.eventbus.api.EventPriority
 import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
 import java.util.*
+
 //import org.slf4j.Logger
 //import org.slf4j.LoggerFactory
 
@@ -49,25 +64,66 @@ class PayAll {
     }
 
     init {
-
-        MinecraftForge.EVENT_BUS.addListener<ClientChatEvent>(EventPriority.HIGHEST) a@{ it ->
-            val split = it.originalMessage.split(" ")
-            if (split.getOrNull(0)?.lowercase()?.startsWith("payall") != true) return@a
-            val dryRun = split[0].lowercase().endsWith("dry")
-            it.isCanceled = true
+        fun handle(delay: Double, amount: Long, vararg cmd: String, dryRun: Boolean = false) {
             job?.takeUnless { !it.isActive || it.isCompleted }?.cancel() ?: let {
-                val delay = split.getDoubleOrUsage(1) ?: return@a
-                val amount = split.getLongOrUsage(2) ?: return@a
-                val cmd = split.subList(3, split.size).takeIf { cmd -> cmd.isNotEmpty() }
-                    ?.joinToString(separator = " ") ?: "pay ! $"
+                //val delay = split.getDoubleOrUsage(1) ?: return@a
+                //val amount = split.getLongOrUsage(2) ?: return@a
+                val newCmd = cmd.takeUnless { it.isEmpty() }?.joinToString(separator = " ") ?: "pay ! $"
                 job = scope.launch {
-                    run(cmd, amount, delay, dryRun)
+                    run(newCmd, amount, delay, dryRun)
                 }
-                return@a
+                return
             }
             job = null
             sendMessage("§aStopped PayAll!")
         }
+
+        MinecraftForge.EVENT_BUS.addListener<RegisterClientCommandsEvent>(EventPriority.HIGHEST) {
+            val init: (String, Boolean) -> Unit = { name, dry ->
+                it.dispatcher.register(
+                    LiteralArgumentBuilder.literal<CommandSourceStack>(name).executes { usage(); return@executes 1 }
+                        .then(RequiredArgumentBuilder.argument<CommandSourceStack, Double>(
+                            "delay",
+                            DoubleArgumentType.doubleArg()
+                        ).executes { usage(); return@executes 1 }.then(
+                            RequiredArgumentBuilder.argument<CommandSourceStack, Long>(
+                                "amount",
+                                LongArgumentType.longArg()
+                            ).executes {
+                                handle(
+                                    DoubleArgumentType.getDouble(it, "delay"),
+                                    LongArgumentType.getLong(it, "amount"),
+                                    cmd = emptyArray(),
+                                    dryRun = dry
+                                )
+                                return@executes 1
+                            }.then(RequiredArgumentBuilder.argument<CommandSourceStack, String>("command", StringArgumentType.greedyString()).executes {
+                                handle(
+                                    DoubleArgumentType.getDouble(it, "delay"),
+                                    LongArgumentType.getLong(it, "amount"),
+                                    cmd = StringArgumentType.getString(it, "command").split(" ", "-").toTypedArray(),
+                                    dryRun = dry
+                                )
+                                return@executes 1
+                            })
+                        )
+                        )).also { println("LITERAL: ${it.literal}") }
+
+            }
+            init("payall", false)
+            init("payalldry", true)
+        }
+
+        //MinecraftForge.EVENT_BUS.addListener<ClientChatEvent>(EventPriority.HIGHEST) a@{ it ->
+        //    // ClientCommandHandler.getDispatcher().setConsumer { context, success, result ->  }
+        //    val split = it.originalMessage.split(" ")
+        //    if (split.getOrNull(0)?.lowercase()?.startsWith("payall") != true) return@a
+        //    if (Minecraft.getInstance().gui.chat.recentChat.last() != it.originalMessage) // Check for versions lower than 1.19, which don't add the message when cancelled
+        //        Minecraft.getInstance().gui.chat.addRecentChat(it.originalMessage)
+        //    val dryRun = split[0].lowercase().endsWith("dry")
+        //    it.isCanceled = true
+
+        //}
     }
 
 
